c414a4996883aa3f31f80984a60df9b4f3a5a34d
Selvaraj Maheswaran <maheswaran.selvaraj@live.in>
2019-01-06 22:30:01 +0530

    Revert "Revert "Checkin""
    This reverts commit acaf94c0aa769abebcf6f42b6d5816f61d9e17a4.

--- TravelBookingApp_Angular/src/app/app.module.ts
+++ TravelBookingApp_Angular/src/app/app.module.ts
@@ -5,18 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { UserComponent } from './user/user.component';
+import { HttpClientModule, HttpClient } from '@angular/common/http'
+import { UserService } from './user/service/user-service.service';
+import { EdituserComponent } from './user/edituser/edituser.component';
+import { AdduserComponent } from './user/adduser/adduser.component';
+import { DeleteuserComponent } from './user/deleteuser/deleteuser.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    UserComponent
+    UserComponent,
+    AdduserComponent,
+    EdituserComponent,
+    DeleteuserComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [UserService, HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- TravelBookingApp_Angular/src/app/user/user.component.ts
+++ TravelBookingApp_Angular/src/app/user/user.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { UserService } from './service/user-service.service';
+import { User } from './../user/user';
+import { Console } from '@angular/core/src/console';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { error } from 'util';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
+  providers: [ UserService],
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  public users: User[];
+  public errorMessage: string;
+  public conversionForm : FormGroup;
 
-  ngOnInit() {
+
+  constructor(private _userService: UserService,
+    private fb: FormBuilder) {
+
+   
   }
 
+  ngOnInit() {
+
+    this._userService.GetUsers().subscribe((data) => {
+      this.users = <User[]>data;
+    },
+    error => this.errorMessage = error);   
+  }  
 }
